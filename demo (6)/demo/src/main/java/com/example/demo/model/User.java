110b2c81af9efb7b55625b23aecbd94a8aac747f
unknown <ismail.ouali@ump.ac.ma>
2024-12-10 23:46:39 +0100

    Role entity

---
+++ demo (6)/demo/src/main/java/com/example/demo/model/User.java
@@ -0,0 +1,31 @@
+package com.example.demo.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.Set;
+
+@Entity
+@Data
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    private String password;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(
+            name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Set<Role> roles;
+}
